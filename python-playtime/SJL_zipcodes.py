8aa69ddc5e4ca693fcbeac1a515ba86b2f285704
Samantha Linnett <samanthajo21394@gmail.com>
2017-02-12 15:01:48 -0500

    SJL_zipcodes
    Code like a Girl zip codes practice

---
+++ python-playtime/SJL_zipcodes.py
@@ -0,0 +1,14 @@
+studybuddy_1 = 'Alison McCauley, 1234 County Line Road, Skaneateles, NY 13152'
+studybuddy_2 = 'Dee Cater, 555 Bradford Pkwy, Syracuse, NY 13224'
+studybuddy_3 = 'Andrea Bianchi, 987 South St, Jamesville, NY 13078'
+studybuddy_4 = 'Kristen Link Logan, 264 Craig Street, Syracuse, NY 13211'
+studybuddy_5 = 'Nina Verity, 111 Bridge St, Solvey, NY 13209'
+
+print "Our study group included people from these zip codes: {0}, {1}, {2}, {3}, and {4}.".format(studybuddy_1[56:], studybuddy_2[42:], studybuddy_3[44:], studybuddy_4[50:], studybuddy_5[38:])
+
+print "Alison lives in {0}.".format(studybuddy_1[56:])
+print "Dee live in {0}.".format(studybuddy_2[42:])
+print "Andrea lives in {0}.".format(studybuddy_3[44:])
+print "Kristen lives in {0}.".format(studybuddy_4[50:])
+print "Nina lives in {0}.".format(studybuddy_5[38:])
+
